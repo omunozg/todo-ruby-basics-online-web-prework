93cfd6e7631de9d2bbf3d3bbc0f99e98fdbd8fc8
Oscar Munoz <omunozg@emory.edu>
2019-03-05 05:42:02 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -11,18 +11,22 @@ def argue(molk)
   
 end
 
-def greeting
-
+def greeting (greeting, name)
+  
 end
 
 def return_a_value
+  return "Nice"
 
 end
 
 def last_evaluated_value
+  return "expert"
 
 end
 
-def pizza_party
+def pizza_party( q = "cheese")
+  return q
+  
 
 end
